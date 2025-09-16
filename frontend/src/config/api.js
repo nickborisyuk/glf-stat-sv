877b7c24216698ad315ddbb8252d8a87d0bd24c0
nb <nick.borisyuk@gmail.com>
2025-09-16 16:52:13 +0300

    debuging

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -35,7 +35,7 @@ const getEnvironment = () => {
 // Get API configuration for current environment
 export const getApiConfig = () => {
   debugger;
-  console.log("Environment is "+getEnvironment(););
+  console.log("Environment is "+getEnvironment());
 
   const env = getEnvironment();
   return API_CONFIG[env] || API_CONFIG.production;
